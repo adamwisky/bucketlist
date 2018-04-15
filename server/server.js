c5e8d5bf9be7a4aa70f52ee6e7a53d76906e4cbb
adamwisky <>
2018-04-15 14:39:38 -0400

    update

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ app.get('/ping', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/*', (req, res) => {
+  res.send('Unknown page?');
+});
+
 const port = 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
