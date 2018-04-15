9208664dc3d3924a996394904a5e20b4dee6dc13
adamwisky <>
2018-04-15 13:14:53 -0400

    update scripts

--- client/startClient.js
+++ client/startClient.js
@@ -1,3 +1,9 @@
 const { spawn } = require('child_process');
 
+console.log("Attempting to start client...");
+
 const startClient = spawn('serve', ['-s', 'build', '-p', '3000']);
+
+startClient.stdout.on('data', function(data) {
+    console.log(data.toString());
+});
--- server/startServer.js
+++ server/startServer.js
@@ -1,3 +1,9 @@
 const { spawn } = require('child_process');
 
+console.log("Attempting to start server...");
+
 const startServer = spawn('node', ['server.js']);
+
+startServer.stdout.on('data', function(data) {
+    console.log(data.toString());
+});
