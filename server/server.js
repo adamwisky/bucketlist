4280ff6c64af7983fd8fc80402525d5e1a3b9206
adamwisky <>
2018-04-15 15:51:33 -0400

    update server logic

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(`${__dirname}/../client/build`));
+
 app.get('/ping', (req, res) => {
   res.send('Hello World!');
 });
