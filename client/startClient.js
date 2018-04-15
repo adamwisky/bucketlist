199153476ce30865e5337aab747d7f967522c2ab
adamwisky <>
2018-04-15 12:38:58 -0400

    update start scripts

--- client/startClient.js
+++ client/startClient.js
@@ -1,7 +1,3 @@
 const { spawn } = require('child_process');
 
-process.chdir('./server');
-const startServer = spawn('nodemon', ['server.js']);
-
-process.chdir('../client');
 const startClient = spawn('serve', ['-s', 'build', '-p', '3000']);
---
+++ server/startServer.js
@@ -0,0 +1,3 @@
+const { spawn } = require('child_process');
+
+const startServer = spawn('node', ['server.js']);
